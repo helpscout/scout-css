3d0edc4a7667ccbad0425aa1d4ce941f523263cc
ItsJonQ <itsjonq@gmail.com>
2017-04-07 17:30:22 -0400

    Tested multiple CSS files

--- test.js
+++ test.js
@@ -1,14 +1,26 @@
 const getSeverity = require('./lib/getSeverity');
+const threshold = 200000;
 
-getSeverity('public/styles/hs-app.css').then(results => {
-  const threshold = 200000;
+const message = (results) => {
+  let threshold = 3000;
+  console.log(`Tested: ${results.file}`);
   if (results.score > threshold) {
-    console.log('Regression');
-    console.log('  🔥 CSS has gotten worse!!!');
+    console.log('🔥 Regression');
+    console.log('  🔥 CSS has gotten worse!!! OMG!!! 🔥');
   } else {
-    console.log('CSS is fine…');
+    console.log('⚡️ No Regression');
+    console.log('  CSS has not gotten worse');
   }
   console.log('Severity:');
   console.log('  Threshold:', threshold);
   console.log('  Score:', results.score);
+  console.log('');
+};
+
+getSeverity('public/css/seed-framework.min.css').then(results => {
+  message(results);
+});
+
+getSeverity('public/styles/hs-app.css').then(results => {
+  message(results);
 });
